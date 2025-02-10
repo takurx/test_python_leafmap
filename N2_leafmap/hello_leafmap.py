6131775f443658260bd933f0b54c9d6d070ea86e
Yoshihiro Nakagawa <taku3wzr@gmail.com>
2025-02-10 13:32:44 +0900

    N2, Hello leafmap
    N2, Hello leafmap

--- N2_leafmap/hello_leafmap.py
+++ N2_leafmap/hello_leafmap.py
@@ -8,9 +8,9 @@ m.to_html("./map.html") # generate an HTML file
 #   File "f:\211220_nakagawa\test_python\N2_leafmap\hello_leafmap.py", line 1, in <module>
 #    import leafmap
 #ModuleNotFoundError: No module named 'leafmap'
-
+#
 # pip3 insall leafmap
-
+#
 # Leafmapを使用してデータを地図上に可視化 #Python - Qiita
 # https://qiita.com/wooooo/items/f905899654e551daaf19
 
