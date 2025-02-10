0ca84a4b0e4c2d57fbe7211eb85b9aba0dd0f830
Yoshihiro Nakagawa <taku3wzr@gmail.com>
2025-02-10 13:30:59 +0900

    N1, Hello world
    N1, Hello world

---
+++ N1_hello_world/hello_world.py
@@ -0,0 +1,5 @@
+print("hello world")
+
+# PS C:\Users\Admin> & C:/Users/Admin/AppData/Local/Microsoft/WindowsApps/python3.12.exe f:/test_python/hello_world.py
+# hello world
+
---
+++ N2_leafmap/hello_leafmap.py
@@ -0,0 +1,16 @@
+import leafmap
+#m = leafmap.Map(center=(40, -100), zoom=4) # イギリス
+m = leafmap.Map(center=(30, 135), zoom=4) # 日本
+#m
+m.to_html("./map.html") # generate an HTML file
+
+#Traceback (most recent call last):
+#   File "f:\211220_nakagawa\test_python\N2_leafmap\hello_leafmap.py", line 1, in <module>
+#    import leafmap
+#ModuleNotFoundError: No module named 'leafmap'
+
+# pip3 insall leafmap
+
+# Leafmapを使用してデータを地図上に可視化 #Python - Qiita
+# https://qiita.com/wooooo/items/f905899654e551daaf19
+
